dbb545a44db2d4069658e84004917c2a805efb69
Mur2 <murtuza.bohra@research.iiit.ac.in>
2019-10-19 17:32:19 +0530

    Add files via upload

--- Kickstart/Practice_KS19_Q3.py
+++ Kickstart/Practice_KS19_Q3.py
@@ -11,13 +11,21 @@ def kickstart_alarm(A,K):
 	N = len(A)
 	for itr_A in range(1,N+1):
 		for x in range(1,N+1):
-			if(itr_A-x >= 0):
-				sum_power =  (sum_power + (A[itr_A-1] * (N-itr_A+1) * ((x*(cal_power(x,K)-1))/(x-1))) )% modulo
+			if(itr_A-x >= 0 and x>1):
+				sum_power =  (sum_power + (A[itr_A-1] * (N-itr_A+1) * (((x/(x-1))*(cal_power(x,K)-1)))) )% modulo
+				# sum_power =  (sum_power + (A[itr_A-1]  * ((cal_power(x-1,modulo-2))*(cal_power(x,K+1)-1))) )% modulo
+
+			else:
+				sum_power =  (sum_power + (A[itr_A-1] * (N-itr_A+1) * K ))% modulo
+				# sum_power =  (sum_power + (A[itr_A-1] * K ))% modulo
+
 	return sum_power
 
 T = int(input())
 for itr in range(T):
-	N,K,x,y,C,D,E1,E2,F = input().split()
+	input_list = input().split()
+	N,K,x,y,C,D,E1,E2,F = [int(input_list[q]) for q in range(len(input_list))]
+
 	A=[]
 	A.append(x+y)
 	for i in range(1,N):
