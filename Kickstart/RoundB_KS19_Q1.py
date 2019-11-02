d1f8c4544572f00b1ddcc894e0462a46e8f699fd
Mur2 <murtuza.bohra@research.iiit.ac.in>
2019-11-02 19:01:00 +0530

    Add files via upload

--- Kickstart/RoundB_KS19_Q1.py
+++ Kickstart/RoundB_KS19_Q1.py
@@ -1,4 +1,7 @@
-
+# Anna has a row of N blocks, each with exactly one letter from A to Z written on it. The blocks are numbered 1, 2, ..., N from left to right.
+# Today, she is learning about palindromes. A palindrome is a string that is the same written forwards and backwards. For example, ANNA, RACECAR, AAA and X are all palindromes, while AB, FROG and YOYO are not.
+# Bob wants to test how well Anna understands palindromes, and will ask her Q questions. The i-th question is: can Anna use all of the blocks numbered from Li to Ri, inclusive, rearranging them if necessary, to form a palindrome? After each question, Anna puts the blocks back in their original positions.
+# Please help Anna by finding out how many of Bob's questions she can answer "yes" to.
 class Node:
 	def __init__(self):
 		self.set_odd_char = set()
@@ -17,8 +20,8 @@ class SegTree:
 		else:
 			node.left = Node()
 			node.right = Node()
-			set1 = self.create_seg_tree(A, l, int(r/2), node.left)
-			set2 = self.create_seg_tree(A, int(r/2)+1, r, node.right)
+			set1 = self.create_seg_tree(A, l, int((r+l)/2), node.left)
+			set2 = self.create_seg_tree(A, int((r+l)/2)+1, r, node.right)
 			node.set_odd_char = set1.union(set2) - set1.intersection(set2)
 
 		node.l_indx = l
@@ -32,7 +35,7 @@ class SegTree:
 				return node.set_odd_char
 			else:
 				if node.left.r_indx >= r:
-					return self.cget_answer(l, r, node.left)
+					return self.get_answer(l, r, node.left)
 				else:
 					set1 = self.get_answer(l, node.left.r_indx, node.left)
 					set2 = self.get_answer(node.left.r_indx+1, r, node.right)
@@ -61,6 +64,6 @@ for itr in range(T):
 	for q in range(Q):
 		l,r = [int(ele)-1 for ele in input().split()]
 		temp = tree.get_answer(l, r, tree.root)
-		if len(temp)==0:
+		if len(temp)<=1:
 			count+=1
 	print('Case #'+str(itr+1)+': '+str(count))
